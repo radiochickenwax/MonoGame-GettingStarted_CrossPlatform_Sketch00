d263eb80d042c7de49f2c7180ee443ccdd68ba10
gregorio.jp.mitchell@gmail.com <gregorio.jp.mitchell@gmail.com>
2018-12-09 09:05:54 -0700

    Start from http://www.monogame.net/documentation/?page=creating_a_new_project_vs and http://www.monogame.net/documentation/?page=adding_content

--- MonoGame-GettingStarted_CrossPlatform_Sketch00/Game1.cs
+++ MonoGame-GettingStarted_CrossPlatform_Sketch00/Game1.cs
@@ -9,6 +9,7 @@ namespace MonoGame_GettingStarted_CrossPlatform_Sketch00
     /// </summary>
     public class Game1 : Game
     {
+        Texture2D textureBall;
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         
@@ -41,6 +42,7 @@ namespace MonoGame_GettingStarted_CrossPlatform_Sketch00
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            textureBall = Content.Load<Texture2D>("ball");
         }
 
         /// <summary>
@@ -76,6 +78,9 @@ namespace MonoGame_GettingStarted_CrossPlatform_Sketch00
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(textureBall, new Vector2(0, 0), Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
